f8b389ef4fbc4c6eea25af8b17e335b4b570b1fd
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2021-05-09 10:53:49 -0700

    added flask-security-too module to extension.py

--- api/extensions.py
+++ api/extensions.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_security import Security, SQLAlchemyUserDatastore
 
 db = SQLAlchemy()
 ma = Marshmallow()
+
+user_datastore = SQLAlchemyUserDatastore
+security = Security()
